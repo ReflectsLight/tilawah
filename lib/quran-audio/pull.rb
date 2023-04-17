8680bb6c6ee4d97ed0cece94522dcbc5c1cf1d96
0x1eef <0x1eef@protonmail.com>
2023-04-17 10:15:57 -0300

    Pull: unify its CLI in a single Ryo object

--- lib/quran-audio/pull.rb
+++ lib/quran-audio/pull.rb
@@ -11,38 +11,36 @@ class Pull
   attr_reader :http,
               :options
 
-  def self.parse_cli(argv)
-    op = nil
-    options = cli_defaults
-    OptionParser.new(nil, 26, " " * 2) do |o|
-      op = o
-      op.banner = "Usage: quran-audio pull [OPTIONS]"
-      op.on("-h", "--help", "Show help") { puts(op.help).then { exit } }
-      cli_options.each { op.on(*_1) }
-    end.parse(argv, into: options).then { options }
-  end
-
-  def self.cli_options
-    [
-      ["-r NAME", "--reciter NAME", "The name of a reciter"],
-      ["-b BITRATE", "--bitrate BITRATE", "MP3 bitrate"],
-      ["-s SURAHS", "--surah SURAHS", "Comma-separated list of surah numbers", Array],
-      ["-c SECONDS", "--cooldown SECONDS", "Cooldown period between requests", Float],
-    ]
-  end
-
-  def self.cli_defaults
-    Ryo(
-      reciter: "alafasy",
-      bitrate: nil,
-      surah: (1..114).to_a,
-      cooldown: 0.5
-    )
+  def self.cli
+    Ryo.from({
+      options: [
+        ["-r NAME", "--reciter NAME", "The name of a reciter"],
+        ["-b BITRATE", "--bitrate BITRATE", "MP3 bitrate"],
+        ["-s SURAHS", "--surah SURAHS", "Comma-separated list of surah numbers", Array],
+        ["-c SECONDS", "--cooldown SECONDS", "Cooldown period between requests", Float],
+      ],
+      defaults: {
+        reciter: "alafasy",
+        bitrate: nil,
+        surah: (1..114).to_a,
+        cooldown: 0.5
+      },
+      parse: Ryo.fn { |argv|
+        op = nil
+        into = Ryo.dup(defaults)
+        OptionParser.new(nil, 26, " " * 2) do |o|
+          op = o
+          op.banner = "Usage: quran-audio pull [OPTIONS]"
+          op.on("-h", "--help", "Show help") { puts(op.help).then { exit } }
+          options.each { op.on(*_1) }
+        end.parse(argv, into:).then { into }
+      }
+    })
   end
 
   def initialize(argv)
     @http = Net::HTTP.new("everyayah.com", 443).tap { _1.use_ssl = true }
-    @options = self.class.parse_cli(argv)
+    @options = Pull.cli.parse.(argv)
   end
 
   def pull(surah_no, ayah_no)
